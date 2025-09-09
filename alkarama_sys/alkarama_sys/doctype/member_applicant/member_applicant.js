bc5fee29513f4d9e0d1c0a6007b2171de34ebd23
armteam1 <ratina.plugins+apjvs@gmail.com>
2025-09-09 14:56:37 +0300

    Initial commit

---
+++ alkarama_sys/alkarama_sys/doctype/member_applicant/member_applicant.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, team@ratina.co and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Member Applicant", {
+// 	refresh(frm) {
+
+// 	},
+// });
